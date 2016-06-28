7874943ba2d4d2ba904a999f854200ea3770a628
ricardojava <ricardobatistaalmeida@gmail.com>
2016-06-28 15:24:10 -0300

    commit ini

---
+++ asl.manualrefundcalculation /src/main/java/com/avianca/ManualRefundCalculationApplication.java
@@ -0,0 +1,12 @@
+package com.avianca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManualRefundCalculationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManualRefundCalculationApplication.class, args);
+	}
+}
