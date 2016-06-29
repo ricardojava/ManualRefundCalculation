9ad10c6d11739987d85067777d16d4f7fe3926fc
ricardojava <ricardobatistaalmeida@gmail.com>
2016-06-29 15:25:01 -0300

    create class config route rest

---
+++ asl.manualrefundcalculation /src/main/java/rest/JerseyConfig.java
@@ -0,0 +1,16 @@
+package rest;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by wallace on 17/06/16.
+ */
+@Component
+public class JerseyConfig extends ResourceConfig {
+
+    public JerseyConfig() {
+        packages("com.avianca.asl.rest");
+    }
+
+}
